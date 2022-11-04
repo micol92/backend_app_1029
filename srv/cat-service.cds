10484a278bb78cbbc3507a764bbaa43004762925
Jungwoo Han <jungwoo.han@sap.com>
2022-11-04 07:37:44 +0000

    send

--- srv/cat-service.cds
+++ srv/cat-service.cds
@@ -1,5 +1,19 @@
 using my.bookshop as my from '../db/data-model';
 
-service CatalogService @(requires: 'authenticated-user') {
-    @readonly entity Books as projection on my.Books;
+service CatalogService @(requires: 'any') {
+    entity Books as projection on my.Books ;
+    
+    action SaveBook ();
+
+
+    type BookTypes : {
+        ID : Integer;
+        title  : String;
+        stock  : Integer;
+    }
+
+    action SaveBookTypesEntityProc (inputData : BookTypes) returns Integer;
+
+
 }
+//annotate CatalogService.Books with @odata.draft.enabled;
--- srv/src/main/java/customer/demo02/handlers/CatalogServiceHandler.java
+++ srv/src/main/java/customer/demo02/handlers/CatalogServiceHandler.java
@@ -4,6 +4,17 @@ import java.util.stream.Stream;
 
 import org.springframework.stereotype.Component;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
 import com.sap.cds.services.cds.CdsService;
 import com.sap.cds.services.handler.EventHandler;
 import com.sap.cds.services.handler.annotations.After;
@@ -12,15 +23,105 @@ import com.sap.cds.services.handler.annotations.ServiceName;
 import cds.gen.catalogservice.CatalogService_;
 import cds.gen.catalogservice.Books;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import com.sap.cds.services.cds.CdsService;
+import com.sap.cds.services.handler.EventHandler;
+import com.sap.cds.services.handler.annotations.On;
+import com.sap.cds.services.handler.annotations.ServiceName;
+import org.springframework.jdbc.core.SqlParameter;
+import org.springframework.jdbc.core.SqlReturnResultSet;
+import org.springframework.jdbc.core.CallableStatementCreator;
+import cds.gen.catalogservice.*;
+
+import com.sap.cds.Result;
+import com.sap.cds.Struct;
+import com.sap.cds.ql.Insert;
+import com.sap.cds.ql.Select;
+import com.sap.cds.ql.Update;
+import com.sap.cds.ql.cqn.CqnAnalyzer;
+import com.sap.cds.ql.cqn.CqnSelect;
+import com.sap.cds.ql.cqn.CqnInsert;
+
+import com.sap.cds.reflect.CdsModel;
+import com.sap.cds.services.ErrorStatuses;
+import com.sap.cds.services.ServiceException;
+import com.sap.cds.services.cds.CdsReadEventContext;
+import com.sap.cds.services.cds.CqnService;
+import com.sap.cds.services.draft.DraftService;
+import com.sap.cds.services.handler.EventHandler;
+import com.sap.cds.services.handler.annotations.After;
+import com.sap.cds.services.handler.annotations.Before;
+import com.sap.cds.services.handler.annotations.On;
+import com.sap.cds.services.handler.annotations.ServiceName;
+import com.sap.cds.services.messages.Messages;
+import com.sap.cds.services.persistence.PersistenceService;
+import static cds.gen.catalogservice.CatalogService_.BOOKS;
+
 @Component
 @ServiceName(CatalogService_.CDS_NAME)
 public class CatalogServiceHandler implements EventHandler {
+	//private final PersistenceService db;
 
-	@After(event = CdsService.EVENT_READ)
+   // CatalogServiceHandler() {}
+
+//    @On(entity = Books_.CDS_NAME)
+    @On(event = SaveBookContext.CDS_NAME)
+	public void onSaveBook() {
+
+        System.out.println("zzzzzzzzzzzzzzzzzzzz1");
+        Map<String, Object> book = new HashMap<>();
+        book.put("ID", 101);
+        book.put("title", "Capire 2");
+        book.put("stock", 100);
+        System.out.println("zzzzzzzzzzzzzzzzzzzz2");
+
+        CqnInsert insert = Insert.into("my.Books").entry(book);
+        System.out.println("zzzzzzzzzzzzzzzzzzzz3");
+
+	}  
+
+	@After(event = CqnService.EVENT_READ)
 	public void discountBooks(Stream<Books> books) {
-		books.filter(b -> b.getTitle() != null && b.getStock() != null)
-		.filter(b -> b.getStock() > 200)
-		.forEach(b -> b.setTitle(b.getTitle() + " (discounted)"));
+		books.filter(b -> b.getTitle() != null).forEach(b -> {
+			//loadStockIfNotSet(b);
+			//discountBooksWithMoreThan111Stock(b);
+		});
 	}
+    
+/*
+    @On(event = SaveBookTypesEntityProcContext.CDS_NAME)
+    public void onSaveBookTypesEntityProc(SaveBookTypesEntityProcContext context) {
+
+        
+        Integer pId = 1 ; //context.Id();
+		String pTitle = "tttt"; //context.Title();
+		Integer pStock = 1; //context.Stock();
+
+        Collection<BookTypes> v_result = new ArrayList<>();
+
+		//String bookId = (String) analyzer.analyze(context.getCqn()).targetKeys().get(Books.ID);
+
+		cds.gen.catalogservice.Books books = cds.gen.catalogservice.Books.create();
+		books.setId(pId);
+        books.setTitle(pTitle);
+        books.setStock(pStock); 
+        
+		Result res = db.run(Insert.into(CatalogService_.BOOKS).entry(books));
+		cds.gen.catalogservice.Books inserted = res.single(cds.gen.catalogservice.Books.class);
+
+        context.setResult(1);
+		//messages.success(MessageKeys.REVIEW_ADDED);
+
+		//context.setResult(Struct.access(inserted).as(Books.class));
+
+
+        }    
+*/
 
 }
